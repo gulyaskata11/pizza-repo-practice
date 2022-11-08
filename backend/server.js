dac4d079b1f0505593c966d96d176d74cd4ff923
vombat11 <gulyas.kata11@gmail.com>
2022-11-08 21:43:32 +0100

    add script

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,26 @@ const fs = require('fs')
 
 const app = express()
 
-app.use(express.json()) // IMPORTANT
+app.use(express.json()) 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
 })
 
+app.use('/img', express.static(`${__dirname}/data/img`))
+
 app.use('/public', express.static(`${__dirname}/../frontend/public`))
 
-
-
-
+app.get('/pizza', (req, res) => {
+    fs.readFile(`${__dirname}/data/pizza.json`, (err, data) => {
+      if (err) {
+        console.log('hiba:', err)
+        res.status(500).send('hibavan')
+      } else {
+        res.status(200).send(JSON.parse(data))
+      }
+    })
+  })
 
 
 app.listen(2000, console.log('server listening on http://127.0.0.1:2000'))
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,10 +1,38 @@
 const rootElement = document.querySelector('#root')
 
-const pizzaComponent = (name, price, ingredients, id) => `
-  <div class="beer">
+const pizzaComponent = (name, ingredients, price) => `
+  <div class="pizza">
     <h2>${name}</h2>
-    <h3>${price}</h3>
-    <h4>${rating}</h4>
-    <button class="edit beer${id}">edit</button>
+    <h5>${price} $</h5>
+    <h3>Ingredients: ${ingredients}</h3>
+    <img src="img/${name}.webp" width="24" height="39" alt="pizza">
+    <label for="amount">Amount: </label>
+    <input class="amount" type="number" name="amount">
+    <button class="add-order">Addition</button>
   </div>
 `
+
+fetch('/pizza')
+  .then(res => res.json())
+  .then(pizzas => {
+    pizzas.map((pizza) => rootElement.insertAdjacentHTML('beforeend', pizzaComponent(pizza.name, pizza.ingredients, pizza.price)))
+    
+    const addOrder = document.querySelectorAll('.add-order')
+    
+    for(let btn of addOrder) {
+      btn.addEventListener("click", function(event) {
+       // console.log(event)
+        const pizzaName = event.path[1].children[0].innerText
+       // console.log(pizzaName)
+        const number = event.path[1].children[5].value
+       // console.log(number)
+        const price = event.path[1].children[1].innerText
+        rootElement.insertAdjacentHTML("beforeend", `
+          <div>
+            <h1>${pizzaName}</h1>
+            <h2>${number} x ${price}</h2>
+          </div>
+        `)     
+      })
+    }
+}) 
